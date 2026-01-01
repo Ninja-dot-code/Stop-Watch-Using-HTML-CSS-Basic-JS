0b52761130f92c8d4c1fcfa3b74c8654aa9aec0d
Ninja-dot-code <nazeemakbar018@gmail.com>
2026-01-01 22:48:34 +0500

    Change in varible name and cases

--- logic.js
+++ logic.js
@@ -1,34 +1,35 @@
 let watch = document.querySelector('.timebox');
-let star = document.querySelector('.Start');
-let Stop = document.querySelector('.Stop');
-let reset = document.querySelector('.Reset');
+let StarBTN = document.querySelector('.Start');
+let StopBTN = document.querySelector('.Stop');
+let ResetBTN = document.querySelector('.Reset');
 
 // time varible setting for mili second sec minite\
  let milisec= 0;
  let sec = 0;
  let min = 0;
- let timeid =null;
- star.addEventListener('click',(e)=>{
-    if(timeid !== null){
-        clearInterval(timeid)
+ let TimeId =null;
+ StarBTN.addEventListener('click',(e)=>{
+    if(TimeId !== null){
+        clearInterval(TimeId)
     }
 
-   timeid = setInterval(starrunningtime,10);
+   TimeId = setInterval(starrunningtime,10);
 
  })
 
 
- Stop.addEventListener('click',(e)=>{
-    clearInterval(timeid)
+ StopBTN.addEventListener('click',(e)=>{
+    clearInterval(TimeId)
 
  })
         
   
 
 
- reset.addEventListener('click',(e)=>{
-    if(timeid !== null){
-        clearInterval(timeid)
+ ResetBTN.addEventListener('click',(e)=>{
+    if(TimeId !== null){
+
+        clearInterval(TimeId)
         min =0; sec = 0; milisec = 0;
         watch.textContent = `00 : 00 : 00`
         
@@ -41,10 +42,14 @@ let reset = document.querySelector('.Reset');
 
  function starrunningtime(){
     milisec++;
+
     if(milisec == 100 ){
+
         milisec=0;
         sec++;
+
         if(sec == 60 ){
+
             sec=0;
             min++;
 
@@ -52,6 +57,7 @@ let reset = document.querySelector('.Reset');
         }
        
     }
+    
      
     let milistring = milisec < 10? `0${milisec}`:milisec;
     let secstring = sec < 10 ? `0${sec}` :sec;
